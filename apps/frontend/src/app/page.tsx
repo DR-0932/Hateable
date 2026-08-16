803b4c4cb8369483b1e52d0aafbc3f98950adae5
Dhruv <dhruvrajput757@gmail.com>
2026-08-16 23:59:50 +0530

    added landing page structure

---
+++ apps/frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { HeroSection } from '@/components/marketing/heroSection'
+import React from 'react'
+
+const page = () => {
+  return (
+    <main>
+      <HeroSection/>
+    </main>
+  )
+}
+
+export default page
