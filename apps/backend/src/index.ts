f09e0dcb0abee9a99f576f6d7f4d8419ae99d440
Dhruv <dhruvrajput757@gmail.com>
2026-08-22 23:32:23 +0530

    refactor(backend): update imports in index.ts for new route locations

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,23 @@
 import express from 'express';
+import cors from 'cors';
+import agent_router from './routes/agentRoutes.js'
 
 const app =express();
 
+
+
+app.use(cors({
+    origin:"http://localhost:3000",
+    credentials:true,
+}));
+
+
 app.use(express.json());
 
+app.use("/",agent_router)
 
+const PORT = process.env.PORT || 4000;
 
-
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+})
